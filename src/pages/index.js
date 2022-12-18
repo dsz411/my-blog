b6bffc8d7b8bd1262d7f3077467d70f199e0556b
dsz411 <ynkwyagg19980411@gmail.com>
2022-12-19 01:42:30 +0800

    s

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/blog/vs-code-shortcuts">
+            to="/blog">
             See my blog 😁
           </Link>
         </div>
